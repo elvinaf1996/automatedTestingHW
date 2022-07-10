91618fbf2e4b0595b95bc340a72b7d42a889e608
Эльвина Фаттахова <99656190+elvinaf1996@users.noreply.github.com>
2022-07-11 02:31:45 +0300

    Add files via upload

---
+++ homebrandofficial/src/test/java/productSearchNameTest.java
@@ -0,0 +1,47 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class productSearchNameTest {
+    private WebDriver driver;
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\elvin\\Desktop\\учеба.работа\\мфти\\автоматизация\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://homebrandofficial.ru/wear");
+    }
+
+    @Test
+    public void productSearchNameTest() {
+        String productName = "ФУТБОЛКА ПОЛО ЧЕРНАЯ (М)";
+        driver.findElement(By.cssSelector(".t-store__filter__search .t-store__filter__input")).sendKeys(productName);
+        driver.findElement(By.cssSelector(".t-store__search-icon")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        By element = By.xpath("//*[contains(@class, \"t-store__card__title\")]");
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(element));
+
+        String actualProductName = driver.findElement(element).getText();
+
+        Assert.assertEquals(productName, actualProductName);
+    }
+
+
+    @After
+    public void afterTest() {
+        driver.quit();
+    }
+
+}
